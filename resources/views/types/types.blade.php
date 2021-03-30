196352960c5c6e8d0b06f9b409eddfe5a6fc23b7
borismeeuh <kams0007@hz.nl>
2021-03-30 10:01:59 +0200

    Home page and types page
    Includes the home page, types page and the css.

---
+++ resources/views/types/types.blade.php
@@ -0,0 +1,90 @@
+@extends('common.master')
+
+@section('content')
+
+    <div class="row" id="upperRow">
+        <div class="column" id="typeBox1">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris dolor, feugiat quis
+            nunc eu, condimentum tempus ligula.
+            Sed sed erat nec ex iaculis pharetra. Proin sodales elit malesuada, gravida ipsum sollicitudin,
+            varius elit. Duis laoreet fermentum
+            ante ac fermentum. Phasellus ante erat, fermentum ut lectus sed, gravida
+            fermentum metus. Nullam fringilla at eros vel dapibus. Proin mattis cursus erat vel egestas.
+            <br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox2">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris dolor, feugiat quis
+            nunc eu, condimentum tempus ligula.
+            Sed sed erat nec ex iaculis pharetra. Proin sodales elit malesuada, gravida ipsum sollicitudin,
+            varius elit. Duis laoreet fermentum
+            ante ac fermentum. Phasellus ante erat, fermentum ut lectus sed, gravida
+            fermentum metus. Nullam fringilla at eros vel dapibus. Proin mattis cursus erat vel egestas.
+            <br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox3">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris dolor, feugiat quis
+            nunc eu, condimentum tempus ligula.
+            Sed sed erat nec ex iaculis pharetra. Proin sodales elit malesuada, gravida ipsum sollicitudin,
+            varius elit. Duis laoreet fermentum
+            ante ac fermentum. Phasellus ante erat, fermentum ut lectus sed, gravida
+            fermentum metus. Nullam fringilla at eros vel dapibus. Proin mattis cursus erat vel egestas.
+            <br><br><br><br>
+        </div>
+    </div>
+
+    <div class="row" id="middleRow">
+        <div class="column" id="typeBox4">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox5">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox6">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+    </div>
+
+    <div class="row" id="lowerRow">
+        <div class="column" id="typeBox7">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox8">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+
+        <div class="column" id="typeBox9">
+            <img src="man.png" alt="test" class="logo">
+            <h1><strong>Type</strong></h1>
+            Placeholder description
+            <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+    </div>
+
+
+@endsection
