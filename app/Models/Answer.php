0e171fe3d642b5e77e229c17fb9e9212cc8fd917
orizzzar <70523219+orizzzar@users.noreply.github.com>
2021-04-03 13:10:43 +0200

    Relationships created

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+use App\Models\Result;
 
 class Answer extends Model
 {
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function results()
+    {
+        return $this->belongsToMany(Result::class)->withPivot('coefficient');
+    }
+
     use HasFactory;
 }
