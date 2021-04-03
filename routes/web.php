4bd933a594c3b2b45b420abd2409f6ebedb6a2a6
orizzzar <70523219+orizzzar@users.noreply.github.com>
2021-04-03 13:52:42 +0200

    Endpoints to view & clear session

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\TypesController;
 use App\Http\Controllers\QuizController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,8 +29,21 @@ Route::get('/result', function () {
   return view('result');
 });
 
+// Quiz
+
 Route::get('/quiz', [QuizController::class, 'index']);
 Route::post('/quiz/answer', [QuizController::class, 'answer']);
 Route::post('/quiz/back', [QuizController::class, 'back']);
 Route::post('/quiz/restart', [QuizController::class, 'restart']);
 Route::post('/quiz/current', [QuizController::class, 'current']);
+
+// Session
+
+Route::get('/ses', function (Request $request) {
+  return $request->session()->all();
+});
+
+Route::get('/delses', function (Request $request) {
+  $request->session()->invalidate();
+  return redirect('/ses');
+});
