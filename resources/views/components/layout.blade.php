21dc94f7b25aebba25f13046c9a0aebe4be2cf59
borismeeuh <kams0007@hz.nl>
2021-04-22 13:29:07 +0200

    changed css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,25 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Complete the quiz</title>
-    
+
     {{-- FONTS --}}
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Chango&family=Syne+Mono&display=swap" rel="stylesheet">
-    
+
     {{-- JS --}}
     <script src="{{ asset('mix/js/app.js') }}"></script>
 
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('mix/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('mix/css/main.css') }}">
 
 </head>
 <body>
 
-    @csrf
+<header>
+	<img src="{{ asset('img/logo-updated.svg') }}" alt="">
+</header>
 
-    <header>
-        <img src="{{ asset('img/logo-updated.svg') }}" alt="">
-    </header>
+
+    @csrf
 
     {{ $slot }}
 
