5048147194159c51bdcdc4c2aca0ac72716b2efb
orizzzar <70523219+orizzzar@users.noreply.github.com>
2021-04-23 01:07:55 +0200

    few design fixes

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -3,7 +3,7 @@
         <div class="banner">
             <div class="banner-inner">
                 <h1>Welcome to the HZ student quiz</h1>
-                <p>Take this test to find out what type of student you are!</p>
+                <p>Take this test to find out what type of student you are</p>
                 <a class="baton" href="/quiz">start</a>
             </div>
         </div>
