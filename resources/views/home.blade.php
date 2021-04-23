3a07a0d857981d70423eda66f0be2c9bfb6ec949
orizzzar <70523219+orizzzar@users.noreply.github.com>
2021-04-23 02:21:36 +0200

    clean up 2 fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,9 @@
 <x-layout>
-    <main>
-        <div class="banner">
-            <div class="banner-inner">
-                <h1>Welcome to the HZ student quiz</h1>
-                <p>Take this test to find out what type of student you are</p>
-                <a class="baton" href="/quiz">start</a>
-            </div>
+    <main class="home-main">
+        <div class="home-content">
+            <h1>Welcome to the HZ student quiz</h1>
+            <p>Take this test to find out what type of student you are</p>
+            <a class="baton" href="/quiz">start</a>
         </div>
     </main>
 </x-layout>
