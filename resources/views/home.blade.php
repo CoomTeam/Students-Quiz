2b8289997ad1ceda29948d13903a0af4c7d56e21
snowbalzz <alek0007@hz.nl>
2021-05-22 14:13:08 +0200

    CSS Setup for the Darkmode

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <div class="content">
             <h1>Welcome to the HZ student Quiz</h1>
             <p>Take this test to find out what type of student you are!</p>
-            <a class="sladkijbatonchik" href="/quiz">start</a>
+            <a class="batonchik" href="/quiz">start</a>
         </div>
 
     </main>
