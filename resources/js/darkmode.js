9c9741992b75bd6c6283b80384e16b96f07da6f0
snowbalzz <alek0007@hz.nl>
2021-05-24 15:53:15 +0200

    Saving preference of theme in localStorage

--- resources/js/darkmode.js
+++ resources/js/darkmode.js
@@ -1,24 +1,42 @@
-console.log('i work');
 
 const tgl = document.getElementById('cb2');
 const body = document.body;
 
-const theme = localStorage.getItem('theme');
+let darkmode = localStorage.getItem('darkmode');
 
+checkLocalStorage();
 
 tgl.onchange = function() {
+    darkmode = localStorage.getItem('')
     if(body.classList.contains('light')){
-        
-        body.classList.replace('light', 'dark');    
-
+        darkToggle();
         return;    
     } 
-
     if(body.classList.contains('dark')){
-
-        body.classList.replace('dark', 'light');        
-        
+        lightToggle();
         return;    
     } 
 
 };
+
+function checkLocalStorage() {
+    if (darkmode === 'on') {
+        //set theme to dark
+        darkToggle();
+        tgl.checked = true;
+    } else {
+        //set theme to light
+        lightToggle();
+        tgl.checked = false;
+    }
+}
+
+function lightToggle() {
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('darkmode', 'off');
+}
+
+function darkToggle() {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('darkmode', 'on');
+}
